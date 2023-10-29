7a9dfca7fea7c3bd343e903b53f5fa974a598ea5
ousr4 <odar.rebenes@proton.me>
2023-10-29 14:48:38 +0100

    Solution to exercises 3.7 and 3.8

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+
+morgan.token('payload', request => {
+    return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :res[content-length] - :response-time ms :payload'))
 
 let persons = [
     {
